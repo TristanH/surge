c7e7da230c9b089cee32dc178c4edb6140bcb535
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-02-27 14:46:34 -0800

    added endpoints for keywords

--- main/views/serializers.py
+++ main/views/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class KeywordSerializer(serializers.HyperlinkedModelSerializer):                   
     class Meta:                                                                    
         model = Keyword                                                            
-        fields = ('string')
+        fields = ('string', 'is_main',)
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
