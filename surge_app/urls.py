bd2cf37d192bc1edc262e3458fad9c428c4337da
Tristan Homsi <tristanhomsi@gmail.com>
2016-02-27 00:21:58 -0800

    fix hello views

--- surge_app/urls.py
+++ surge_app/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
