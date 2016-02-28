b55b76164482fc36daf5d9771db6b13462d46d22
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-02-27 17:33:08 -0800

    another import fix

--- main/views/post_views.py
+++ main/views/post_views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from requests import post
-from request_oauth2 import OAuth2
+from requests_oauth2 import OAuth2
 
 from django.contrib.auth.models import User   
 
