1bfa4bbe26d12e5f645eb74ea6ca9cded812f737
Tristan Homsi <tristanhomsi@gmail.com>
2016-02-27 16:47:25 -0800

    finally get serializers working

--- main/views/serializers.py
+++ main/views/serializers.py
@@ -1,18 +1,33 @@
-from main.models import Keyword, Order, Item
+from main.models import Keyword, Order, Item, KeywordGroup
 from rest_framework import serializers
 
 # Serialization for [all models, for now just Keyword]                             
-class KeywordSerializer(serializers.HyperlinkedModelSerializer):                   
+class KeywordSerializer(serializers.ModelSerializer):                   
     class Meta:                                                                    
         model = Keyword                                                            
         fields = ('string', 'is_main',)
 
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
+class KeywordGroupSerializer(serializers.HyperlinkedModelSerializer):                   
+    tags = KeywordSerializer
+
+    class Meta:                                                                    
+        model = KeywordGroup
+        depth=1
+        fields=('tags',)                                              
+
+class OrderSerializer(serializers.ModelSerializer):
+    # description = serializers.CharField(max_length=511)
+
     class Meta:
         model = Order
-        fields = ('description',)
+        fields=('bidding_end_time', 'keywords', 'latitude', 'longitude', 'description')
+        depth = 1
+        # fields = ('description', 'bidding_end_time', 'keywords')
 
-class ItemSerializer(serializers.HyperlinkedModelSerializer):
+class ItemSerializer(serializers.ModelSerializer):
+    keywords=KeywordGroupSerializer()
+
     class Meta:
         model = Item
-        fields = ('name', 'description')
+        fields=('name', 'restaurant', 'keywords', 'description')
+        depth = 1
