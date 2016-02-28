be5819dabb4ff498b0c2aa51f2c1277ab039d57b
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-02-27 16:14:51 -0800

    more json

--- main/views/serializers.py
+++ main/views/serializers.py
@@ -1,4 +1,4 @@
-from main.models import Keyword, Order
+from main.models import Keyword, Order, Item
 from rest_framework import serializers
 
 # Serialization for [all models, for now just Keyword]                             
@@ -11,3 +11,8 @@ class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
         fields = ('hungry_user',)
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ('name', 'description')
