fd6897fd1dd85f5a8e7c5f3ef4f0c1fbf1529d8d
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-02-27 16:33:38 -0800

    added lyft call

--- main/views/post_views.py
+++ main/views/post_views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from requests import post
 
 from django.contrib.auth.models import User   
 
@@ -13,9 +14,12 @@ from views import get_bidding_orders
 @api_view(['POST'])
 @permission_classes((AllowAny,))
 def call_lyft(request):
-    
+    lat = request.POST['lat']
+    lng = request.POST['lng']
+    l_request = post('https://api.lyft.com/v1/rides', params={'lat' : lat,
+                                                              'lng' : lng,
+                                                              'ride_type' : 'lyft'})
     return Response(status=HTTP_404_NOT_FOUND)
-
 
 @api_view(['GET'])
 @permission_classes((AllowAny,))
@@ -28,8 +32,8 @@ def get_orders(request, pk):
     import json
     new_orders = []
     for dict_i in orders:
-        new_orders.append({"order" : OrderSerializer(dict_i["order"], context={'request': request}).data,
-                           "item"  : ItemSerializer(dict_i["item"], context={'request': request}).data})
+        new_orders.append({"order" : OrderSerializer(dict_i["order"].data),
+                           "item"  : ItemSerializer(dict_i["item"].data)})
     orders_json = json.dumps(new_orders)
     return Response(orders_json, status=status.HTTP_200_OK)
 
@@ -51,6 +55,7 @@ def new_order(request, pk):
     except:
         return Response("Bad request for " + user, status=status.HTTP_400_BAD_REQUEST)
 
+    
     qs = Order.objects.filter(id=order.id)
     serializer = OrderSerializer(qs, many=True)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
